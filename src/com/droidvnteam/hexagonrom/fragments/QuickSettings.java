50afaabcacedbd9a1ebb28f6533f304f489cc5e6
SpiritCroc <spiritcroc@gmail.com>
2018-01-06 12:15:24 +0100

    QS auto brightness icon: no need to restart SystemUI [2/2]

--- src/com/droidvnteam/hexagonrom/fragments/QuickSettings.java
+++ src/com/droidvnteam/hexagonrom/fragments/QuickSettings.java
@@ -17,64 +17,14 @@
 
 package com.droidvnteam.hexagonrom.fragments;
 
-import android.content.ContentResolver;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-
 import com.droidvnteam.hexagonrom.BaseSettingsFragment;
 import com.droidvnteam.hexagonrom.R;
-import com.droidvnteam.hexagonrom.utils.Util;
+import com.aicp.extras.R;
 
-import com.android.internal.util.hex.DeviceUtils;
-
-public class QuickSettings extends BaseSettingsFragment
-    implements OnPreferenceChangeListener {
-
-    private static final String PREF_BRIGHTNESS_ICON_POSITION = "brightness_icon_position";
-    private static final String PREF_QS_STYLE_DARK = "qs_style_dark";
-    private static final String KEY_FPC_QUICK_PULLDOWN = "status_bar_quick_qs_pulldown_fp";
-
-    private SwitchPreference mBrightnessIconPosition;
+public class QuickSettings extends BaseSettingsFragment {
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.quick_settings;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        mBrightnessIconPosition = (SwitchPreference) findPreference(PREF_BRIGHTNESS_ICON_POSITION);
-        mBrightnessIconPosition.setOnPreferenceChangeListener(this);
-
-        mQsStyleDark = (SwitchPreference) findPreference(PREF_QS_STYLE_DARK);
-        mQsStyleDark.setOnPreferenceChangeListener(this);
-
-        mFPQuickPullDown = (SwitchPreference) findPreference(KEY_FPC_QUICK_PULLDOWN);
-        if(!DeviceUtils.deviceSupportsFingerPrint(getActivity())){
-            mFPQuickPullDown.getParent().removePreference(mFPQuickPullDown);
-        }
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mBrightnessIconPosition) {
-            Util.showSystemUiRestartDialog(getContext());
-            return true;
-        } else if (preference == mQsStyleDark) {
-            Util.showSystemUiRestartDialog(getContext());
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        return super.onPreferenceTreeClick(preference);
     }
 }
