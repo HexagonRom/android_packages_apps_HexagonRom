AICP/packages_apps_AicpExtras

Add SwitchBar for system setting use
- Import from Settings
- Remove unneeded device admin/restriction parts
- Use it automatically when opening preference screens from
  SystemSettingMasterSwitchPreference
- Disable all contained preferences of BaseSettingsFragments
  when disabling the switch

Original commit history:

commit 2a77408663206b749fc19171abe3b6f7f039fcf7
Author: Jiaquan He <hejq@google.com>

    Show the highlight for the wifi toggle switch.

commit aa71afe5978b1340a2d702f3a75d22baf8c96348
Author: Fan Zhang <zhfan@google.com>

    Use MetricsFeatureProvider for logging in Settings.

commit 664f6dacca97281e852bbb7fd8094a329d299d9a
Author: Phil Weaver <pweaver@google.com>

    Make sure setting switch bar state is announced.

commit 6d2a0c80df01a95adccfbb5926b1c463b60f6366
Author: Fan Zhang <zhfan@google.com>

    Log switch toggle action whenever its status changes.

commit 0a13cfbf8e9ef08caf83b44d6cd243998b07b004
Merge: a028010c23 6913b6118f
Author: Chris Wren <cwren@android.com>

    Merge \"log as counters instead of histograms\" into nyc-dev
    am: 6913b6118f

commit 1753dfd5cd5aaeb4c5ed9e2e9e10f13f89bf37d2
Author: Chris Wren <cwren@android.com>

    log as counters instead of histograms

commit 8dd11327b65beeadaf25de5caaf04e699753d67c
Author: Andrew Sapperstein <asapperstein@google.com>

    Refactor Settings theme colors.

commit 115d15763d95c2f93a639ad2569037d274d46a22
Author: Sudheer Shanka <sudheersai@google.com>

    Adjust restricted lock positions as per new design.

commit e4ebcd12be7baae58a1fabcda6c7544c911c95eb
Author: Jason Monk <jmonk@google.com>

    Add generic logging to Settings switch bar

commit e88a05601c1a69ca77b05eaab4cc10a0efdf1e05
Author: Anna Galusza <agalusza@google.com>

    Don't read state twice ("on on"/"off off") in Settings Toggle preferences.
    Bug: 26967006

commit 0285c806647ad7e84302044587b56e9d1c918b85
Author: Anna Galusza <agalusza@google.com>

    UI fixes (back arrow, blue switch bar, append whether Talkbac

src/com/aicp/extras/SettingsActivity.java
@@ -24,22 +24,47 @@ import android.os.Bundle;
 
 import com.aicp.extras.fragments.Dashboard;
 import com.aicp.extras.preference.MasterSwitchPreference;
+import com.aicp.extras.preference.SystemSettingMasterSwitchPreference;
+import com.aicp.extras.preference.SystemSettingSwitchBarController;
+import com.aicp.extras.widget.SwitchBar;
 
 public class SettingsActivity extends BaseActivity {
 
     private Fragment mFragment;
+    private SwitchBar mSwitchBar;
 
     // String extra containing the fragment class
     private static final String EXTRA_FRAGMENT_CLASS =
             "com.aicp.extras.extra.preference_fragment";
+    // String extra containing an optional system settings key to be controlled by the switch bar
+    private static final String EXTRA_SWITCH_SYSTEM_SETTINGS_KEY =
+            "com.aicp.extras.extra.preference_switch_system_settings_key";
+    // Default value for switch bar controlling the system setting
+    private static final String EXTRA_SWITCH_SYSTEM_SETTINGS_DEFAULT_VALUE =
+            "com.aicp.extras.extra.preference_switch_system_settings_default_value";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
 
-        String fragmentClass = getIntent().getStringExtra(EXTRA_FRAGMENT_CLASS);
+        Intent mIntent = getIntent();
+
+        String fragmentClass = mIntent.getStringExtra(EXTRA_FRAGMENT_CLASS);
         mFragment = getNewFragment(fragmentClass);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, mFragment).commit();
+        getFragmentManager().beginTransaction().replace(R.id.main_content, mFragment).commit();
+
+        mSwitchBar = (SwitchBar) findViewById(R.id.switch_bar);
+        if (mIntent.hasExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_KEY)) {
+            mSwitchBar.show();
+            BaseSettingsFragment settingsFragment = mFragment instanceof BaseSettingsFragment
+                    ? (BaseSettingsFragment) mFragment : null;
+            new SystemSettingSwitchBarController(mSwitchBar,
+                    mIntent.getStringExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_KEY),
+                    mIntent.getBooleanExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_DEFAULT_VALUE, false),
+                    getContentResolver(),
+                    settingsFragment);
+        }
     }
 
     @Override
@@ -82,8 +107,14 @@ public class SettingsActivity extends BaseActivity {
                 || preference instanceof MasterSwitchPreference) {
             String fragmentClass = preference.getFragment();
             if (fragmentClass != null) {
-                startActivity(new Intent(this, SubSettingsActivity.class)
-                        .putExtra(EXTRA_FRAGMENT_CLASS, fragmentClass));
+                Intent intent = new Intent(this, SubSettingsActivity.class);
+                intent.putExtra(EXTRA_FRAGMENT_CLASS, fragmentClass);
+                if (preference instanceof SystemSettingMasterSwitchPreference) {
+                    intent.putExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_KEY, preference.getKey());
+                    intent.putExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_DEFAULT_VALUE,
+                            ((SystemSettingMasterSwitchPreference) preference).getDefaultValue());
+                }
+                startActivity(intent);
                 return true;
             }
         }

src/com/aicp/extras/preference/SystemSettingSwitchBarController.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.preference;
+
+import android.content.ContentResolver;
+import android.widget.Switch;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.widget.SwitchBar;
+
+public class SystemSettingSwitchBarController implements SwitchBar.OnSwitchChangeListener {
+
+    private String mKey;
+    private SystemSettingsStore mPreferenceDataStore;
+    private BaseSettingsFragment mSettingsFragment;
+
+    public SystemSettingSwitchBarController(SwitchBar switchBar, String key, boolean defaultValue,
+                                            ContentResolver resolver,
+                                            BaseSettingsFragment settingsFragment) {
+        mKey = key;
+        mPreferenceDataStore = new SystemSettingsStore(resolver);
+        mSettingsFragment = settingsFragment;
+        switchBar.addOnSwitchChangeListener(this);
+
+        // Init
+        boolean initialValue = mPreferenceDataStore.getBoolean(mKey, defaultValue);
+        switchBar.setChecked(initialValue);
+        if (mSettingsFragment != null) {
+            mSettingsFragment.setMasterDependencyState(initialValue);
+        }
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        mPreferenceDataStore.putBoolean(mKey, isChecked);
+        if (mSettingsFragment != null) {
+            mSettingsFragment.setMasterDependencyState(isChecked);
+        }
+    }
+
+}

src/com/aicp/extras/widget/ToggleSwitch.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Switch;
+
+public class ToggleSwitch extends Switch {
+
+    private ToggleSwitch.OnBeforeCheckedChangeListener mOnBeforeListener;
+
+    public static interface OnBeforeCheckedChangeListener {
+        public boolean onBeforeCheckedChanged(ToggleSwitch toggleSwitch, boolean checked);
+    }
+
+    public ToggleSwitch(Context context) {
+        super(context);
+    }
+
+    public ToggleSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ToggleSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ToggleSwitch(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void setOnBeforeCheckedChangeListener(OnBeforeCheckedChangeListener listener) {
+        mOnBeforeListener = listener;
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        if (mOnBeforeListener != null
+                && mOnBeforeListener.onBeforeCheckedChanged(this, checked)) {
+            return;
+        }
+        super.setChecked(checked);
+    }
+
+    public void setCheckedInternal(boolean checked) {
+        super.setChecked(checked);
+    }
+}

HexagonRom/android_packages_apps_HexagonRom

src/com/droidvnteam/hexagonrom/SettingsActivity.java
@@ -24,22 +24,47 @@ import android.os.Bundle;
 
 import com.droidvnteam.hexagonrom.fragments.Dashboard;
 import com.droidvnteam.hexagonrom.preference.MasterSwitchPreference;
+import com.droidvnteam.hexagonrom.preference.SystemSettingMasterSwitchPreference;
+import com.droidvnteam.hexagonrom.preference.SystemSettingSwitchBarController;
+import com.droidvnteam.hexagonrom.widget.SwitchBar;
 
 public class SettingsActivity extends BaseActivity {
 
     private Fragment mFragment;
+    private SwitchBar mSwitchBar;
 
     // String extra containing the fragment class
     private static final String EXTRA_FRAGMENT_CLASS =
             "com.droidvnteam.hexagonrom.extra.preference_fragment";
+    // String extra containing an optional system settings key to be controlled by the switch bar
+    private static final String EXTRA_SWITCH_SYSTEM_SETTINGS_KEY =
+            "com.droidvnteam.hexagonrom.extra.preference_switch_system_settings_key";
+    // Default value for switch bar controlling the system setting
+    private static final String EXTRA_SWITCH_SYSTEM_SETTINGS_DEFAULT_VALUE =
+            "com.droidvnteam.hexagonrom.extra.preference_switch_system_settings_default_value";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
 
-        String fragmentClass = getIntent().getStringExtra(EXTRA_FRAGMENT_CLASS);
+        Intent mIntent = getIntent();
+
+        String fragmentClass = mIntent.getStringExtra(EXTRA_FRAGMENT_CLASS);
         mFragment = getNewFragment(fragmentClass);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, mFragment).commit();
+        getFragmentManager().beginTransaction().replace(R.id.main_content, mFragment).commit();
+
+        mSwitchBar = (SwitchBar) findViewById(R.id.switch_bar);
+        if (mIntent.hasExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_KEY)) {
+            mSwitchBar.show();
+            BaseSettingsFragment settingsFragment = mFragment instanceof BaseSettingsFragment
+                    ? (BaseSettingsFragment) mFragment : null;
+            new SystemSettingSwitchBarController(mSwitchBar,
+                    mIntent.getStringExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_KEY),
+                    mIntent.getBooleanExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_DEFAULT_VALUE, false),
+                    getContentResolver(),
+                    settingsFragment);
+        }
     }
 
     @Override
@@ -82,8 +107,14 @@ public class SettingsActivity extends BaseActivity {
                 || preference instanceof MasterSwitchPreference) {
             String fragmentClass = preference.getFragment();
             if (fragmentClass != null) {
-                startActivity(new Intent(this, SubSettingsActivity.class)
-                        .putExtra(EXTRA_FRAGMENT_CLASS, fragmentClass));
+                Intent intent = new Intent(this, SubSettingsActivity.class);
+                intent.putExtra(EXTRA_FRAGMENT_CLASS, fragmentClass);
+                if (preference instanceof SystemSettingMasterSwitchPreference) {
+                    intent.putExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_KEY, preference.getKey());
+                    intent.putExtra(EXTRA_SWITCH_SYSTEM_SETTINGS_DEFAULT_VALUE,
+                            ((SystemSettingMasterSwitchPreference) preference).getDefaultValue());
+                }
+                startActivity(intent);
                 return true;
             }
         }

src/com/droidvnteam/hexagonrom/preference/SystemSettingSwitchBarController.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2017 HexagonRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.droidvnteam.hexagonrom.preference;
+
+import android.content.ContentResolver;
+import android.widget.Switch;
+
+import com.droidvnteam.hexagonrom.BaseSettingsFragment;
+import com.droidvnteam.hexagonrom.widget.SwitchBar;
+
+public class SystemSettingSwitchBarController implements SwitchBar.OnSwitchChangeListener {
+
+    private String mKey;
+    private SystemSettingsStore mPreferenceDataStore;
+    private BaseSettingsFragment mSettingsFragment;
+
+    public SystemSettingSwitchBarController(SwitchBar switchBar, String key, boolean defaultValue,
+                                            ContentResolver resolver,
+                                            BaseSettingsFragment settingsFragment) {
+        mKey = key;
+        mPreferenceDataStore = new SystemSettingsStore(resolver);
+        mSettingsFragment = settingsFragment;
+        switchBar.addOnSwitchChangeListener(this);
+
+        // Init
+        boolean initialValue = mPreferenceDataStore.getBoolean(mKey, defaultValue);
+        switchBar.setChecked(initialValue);
+        if (mSettingsFragment != null) {
+            mSettingsFragment.setMasterDependencyState(initialValue);
+        }
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        mPreferenceDataStore.putBoolean(mKey, isChecked);
+        if (mSettingsFragment != null) {
+            mSettingsFragment.setMasterDependencyState(isChecked);
+        }
+    }
+
+}

src/com/droidvnteam/hexagonrom/widget/ToggleSwitch.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.droidvnteam.hexagonrom.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Switch;
+
+public class ToggleSwitch extends Switch {
+
+    private ToggleSwitch.OnBeforeCheckedChangeListener mOnBeforeListener;
+
+    public static interface OnBeforeCheckedChangeListener {
+        public boolean onBeforeCheckedChanged(ToggleSwitch toggleSwitch, boolean checked);
+    }
+
+    public ToggleSwitch(Context context) {
+        super(context);
+    }
+
+    public ToggleSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ToggleSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ToggleSwitch(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public void setOnBeforeCheckedChangeListener(OnBeforeCheckedChangeListener listener) {
+        mOnBeforeListener = listener;
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        if (mOnBeforeListener != null
+                && mOnBeforeListener.onBeforeCheckedChanged(this, checked)) {
+            return;
+        }
+        super.setChecked(checked);
+    }
+
+    public void setCheckedInternal(boolean checked) {
+        super.setChecked(checked);
+    }
+}