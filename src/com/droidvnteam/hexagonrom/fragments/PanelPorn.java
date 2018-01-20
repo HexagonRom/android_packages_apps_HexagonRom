AICP/packages_apps_AicpExtras

Add Panel porn fragment
Change-Id: I57cd3e5d3953508f7f4c8426d5990fc1a7868658

src/com/aicp/extras/fragments/PanelPorn.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.aicp.extras.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+
+public class PanelPorn extends BaseSettingsFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.panel_porn);
+    }
+}

HexagonRom/android_packages_apps_HexagonRom

src/com/droidvnteam/hexagonrom/fragments/PanelPorn.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2017 HexagonRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.droidvnteam.hexagonrom.fragments;
+
+import android.os.Bundle;
+import android.preference.Preference;
+
+import com.droidvnteam.hexagonrom.BaseSettingsFragment;
+import com.droidvnteam.hexagonrom.R;
+
+public class PanelPorn extends BaseSettingsFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.panel_porn);
+    }
+}