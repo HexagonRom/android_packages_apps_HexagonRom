AICP/packages_apps_AicpExtras

Add SystemSettingColorPickerPreference
Change-Id: I66147b4a69e7ed41867e9f1f4ee63f736accf40a

src/com/aicp/extras/preference/SystemSettingColorPickerPreference.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class SystemSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}

HexagonRom/android_packages_apps_HexagonRom

src/com/droidvnteam/hexagonrom/preference/SystemSettingColorPickerPreference.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2017 HexagonRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.droidvnteam.hexagonrom.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+public class SystemSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}