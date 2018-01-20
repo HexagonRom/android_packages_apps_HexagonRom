AICP/packages_apps_AicpExtras

Call SystemUI restart dialog for brightness icon position
Change-Id: Ib514ca97d247e24090c90567399f6cbb4f09d344

src/com/aicp/extras/fragments/QuickSettings.java
@@ -17,17 +17,46 @@
 
 package com.aicp.extras.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.aicp.extras.BaseSettingsFragment;
 import com.aicp.extras.R;
+import com.aicp.extras.utils.Util;
 
-public class QuickSettings extends BaseSettingsFragment {
+public class QuickSettings extends BaseSettingsFragment
+        implements OnPreferenceChangeListener {
+
+    private static final String PREF_BRIGHTNESS_ICON_POSITION = "brightness_icon_position";
+
+    private SwitchPreference mBrightnessIconPosition;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.quick_settings);
+
+        mBrightnessIconPosition = (SwitchPreference) findPreference(PREF_BRIGHTNESS_ICON_POSITION);
+        mBrightnessIconPosition.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBrightnessIconPosition) {
+            Util.showSystemUiRestartDialog(getContext());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }

HexagonRom/android_packages_apps_HexagonRom

src/com/droidvnteam/hexagonrom/fragments/QuickSettings.java
@@ -17,15 +17,49 @@
 
 package com.droidvnteam.hexagonrom.fragments;
 
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.droidvnteam.hexagonrom.BaseSettingsFragment;
 import com.droidvnteam.hexagonrom.R;
+import com.droidvnteam.hexagonrom.utils.Util;
 
-public class QuickSettings extends BaseSettingsFragment {
+public class QuickSettings extends BaseSettingsFragment
+    implements OnPreferenceChangeListener {
+
+    private static final String PREF_BRIGHTNESS_ICON_POSITION = "brightness_icon_position";
+
+    private SwitchPreference mBrightnessIconPosition;
 
     @Override
     protected int getPreferenceResource() {
         return R.xml.quick_settings;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBrightnessIconPosition = (SwitchPreference) findPreference(PREF_BRIGHTNESS_ICON_POSITION);
+        mBrightnessIconPosition.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBrightnessIconPosition) {
+            Util.showSystemUiRestartDialog(getContext());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
     }
 }