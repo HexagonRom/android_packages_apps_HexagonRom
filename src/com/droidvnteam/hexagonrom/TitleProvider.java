AICP/packages_apps_AicpExtras

Add TitleProvider interface for fragments
Use it in DSLV to forward PreferenceScreen title

Change-Id: Ic933f59052f04aae483f4bb58946d11e0ec4aa0b

src/com/aicp/extras/TitleProvider.java
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras;
+
+public interface TitleProvider {
+    CharSequence getTitle();
+}

src/com/aicp/extras/dslv/ActionListViewSettings.java
@@ -63,6 +63,7 @@ import com.android.internal.util.aicp.DeviceUtils;
 import com.android.internal.util.aicp.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.aicp.extras.R;
+import com.aicp.extras.TitleProvider;
 import com.aicp.extras.utils.SlimShortcutPickerHelper;
 
 import java.io.File;
@@ -72,7 +73,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ActionListViewSettings extends ListFragment implements
-            SlimShortcutPickerHelper.OnPickListener {
+            SlimShortcutPickerHelper.OnPickListener, TitleProvider {
 
     private static final int DLG_SHOW_ACTION_DIALOG   = 0;
     private static final int DLG_SHOW_ICON_PICKER     = 1;
@@ -303,6 +304,29 @@ public class ActionListViewSettings extends ListFragment implements
                 fragmentManager.beginTransaction().remove(fragment).commit();
             }
         }
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        if (mAdditionalFragmentAttached) {
+            FragmentManager fragmentManager = getFragmentManager();
+            Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+            if (fragment instanceof android.preference.PreferenceFragment) {
+                android.preference.PreferenceScreen preferenceScreen =
+                        ((android.preference.PreferenceFragment) fragment).getPreferenceScreen();
+                if (preferenceScreen != null) {
+                    return preferenceScreen.getTitle();
+                }
+            } else if (fragment instanceof android.support.v14.preference.PreferenceFragment) {
+                android.support.v7.preference.PreferenceScreen preferenceScreen =
+                        ((android.support.v14.preference.PreferenceFragment) fragment)
+                                .getPreferenceScreen();
+                if (preferenceScreen != null) {
+                    return preferenceScreen.getTitle();
+                }
+            }
+        }
+        return null;
     }
 
     private void loadAdditionalFragment() {

HexagonRom/android_packages_apps_HexagonRom

src/com/droidvnteam/hexagonrom/TitleProvider.java
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2017 HexagonRom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.droidvnteam.hexagonrom;
+
+public interface TitleProvider {
+    CharSequence getTitle();
+}

src/com/droidvnteam/hexagonrom/dslv/ActionListViewSettings.java
@@ -64,6 +64,7 @@ import com.android.internal.util.hex.DeviceUtils.FilteredDeviceFeaturesArray;
 
 import com.droidvnteam.hexagonrom.R;
 import com.droidvnteam.hexagonrom.utils.SlimShortcutPickerHelper;
+import com.droidvnteam.hexagonrom.TitleProvider;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -72,7 +73,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ActionListViewSettings extends ListFragment implements
-            SlimShortcutPickerHelper.OnPickListener {
+            SlimShortcutPickerHelper.OnPickListener, TitleProvider {
 
     private static final int DLG_SHOW_ACTION_DIALOG   = 0;
     private static final int DLG_SHOW_ICON_PICKER     = 1;
@@ -303,6 +304,29 @@ public class ActionListViewSettings extends ListFragment implements
                 fragmentManager.beginTransaction().remove(fragment).commit();
             }
         }
+    }
+
+    @Override
+    public CharSequence getTitle() {
+        if (mAdditionalFragmentAttached) {
+            FragmentManager fragmentManager = getFragmentManager();
+            Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+            if (fragment instanceof android.preference.PreferenceFragment) {
+                android.preference.PreferenceScreen preferenceScreen =
+                        ((android.preference.PreferenceFragment) fragment).getPreferenceScreen();
+                if (preferenceScreen != null) {
+                    return preferenceScreen.getTitle();
+                }
+            } else if (fragment instanceof android.support.v14.preference.PreferenceFragment) {
+                android.support.v7.preference.PreferenceScreen preferenceScreen =
+                        ((android.support.v14.preference.PreferenceFragment) fragment)
+                                .getPreferenceScreen();
+                if (preferenceScreen != null) {
+                    return preferenceScreen.getTitle();
+                }
+            }
+        }
+        return null;
     }
 
     private void loadAdditionalFragment() {